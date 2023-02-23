3f6b44c30605fe18e6f4b492bf2b40032de2faf7
SMauhourat <santiagomauhourat@hotmail.com>
2023-02-23 08:55:08 -0300

    add json route

--- myApp.js
+++ myApp.js
@@ -1,6 +1,19 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  //res.send("Hello Express");
+  absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath)
+});
+
+app.get("/json", (req, res) => {
+  res.json({message: "Hello json"});
+});
 
 
 
@@ -36,4 +49,4 @@ let app = express();
 
 
 
- module.exports = app;
+module.exports = app;
